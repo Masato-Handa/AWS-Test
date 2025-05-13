f023a747a2548abe092d71dd13217b6da98ab0fe
Masato <71016402+Masato-Handa@users.noreply.github.com>
2025-05-14 00:12:48 +0800

    Update install_launch.sh

--- FlaskApp/install_launch.sh
+++ FlaskApp/install_launch.sh
@@ -1,15 +1,19 @@
 #!/bin/bash -ex
-git clone https://github.com/Masato-Handa/FlaskApplication
-cd FlaskApp/
-yum -y install python3 mysql
-pip3 install -r requirements.txt
-amazon-linux-extras install epel
-yum -y install stress
+# Check if the FlaskApp directory exists
+if [ ! -d "FlaskApp" ]; then
+  git clone https://github.com/Masato-Handa/FlaskApplication
+fi
+
+cd FlaskApplication/FlaskApp/
+sudo yum -y install python3 mysql
+sudo pip3 install -r requirements.txt
+sudo amazon-linux-extras install epel
+sudo yum -y install stress
 export PHOTOS_BUCKET=${group8-employee-photos-bucket}
 export AWS_DEFAULT_REGION=us-east-1
 export DYNAMO_MODE=on
 
 FLASK_APP=application.py /usr/local/bin/flask run --host=0.0.0.0 --port=8080
 
-systemctl enable amazon-ssm-agent
-systemctl start amazon-ssm-agent
+sudo systemctl enable amazon-ssm-agent
+sudo systemctl start amazon-ssm-agent
