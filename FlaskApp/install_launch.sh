e99dc31b26ad028629bd4c2a01da233e9d1fad90
Masato <71016402+Masato-Handa@users.noreply.github.com>
2025-05-14 00:47:59 +0800

    Update install_launch.sh

--- FlaskApp/install_launch.sh
+++ FlaskApp/install_launch.sh
@@ -2,6 +2,7 @@
 
 #install dependencies
 sudo yum -y install python3 
+sudo yum install -y python3-pip
 sudo pip3 install -r requirements.txt
 sudo amazon-linux-extras install epel
 sudo yum -y install stress
