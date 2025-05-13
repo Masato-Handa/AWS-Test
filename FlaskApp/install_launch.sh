0a27083950d187637a4a19a0f38b759c2b13d206
Masato <71016402+Masato-Handa@users.noreply.github.com>
2025-05-13 21:10:09 +0800

    Update install_launch.sh

--- FlaskApp/install_launch.sh
+++ FlaskApp/install_launch.sh
@@ -5,9 +5,11 @@ yum -y install python3 mysql
 pip3 install -r requirements.txt
 amazon-linux-extras install epel
 yum -y install stress
-export PHOTOS_BUCKET=${employeephotosbucket123}
+export PHOTOS_BUCKET=${group8-employee-photos-bucket}
 export AWS_DEFAULT_REGION=us-east-1
 export DYNAMO_MODE=on
-FLASK_APP=application.py /usr/local/bin/flask run --host=0.0.0.0 --port=80
+
+FLASK_APP=application.py /usr/local/bin/flask run --host=0.0.0.0 --port=8080
+
 systemctl enable amazon-ssm-agent
 systemctl start amazon-sim-agent
